40feb94a8a0111dbfa2e8f140e08fcc808e730e0
Monaldcry7788 <63928518+Monaldcry7788@users.noreply.github.com>
2021-03-11 13:06:36 +0100

    Add files via upload

---
+++ Warn/Database/BanSystem.cs
@@ -0,0 +1,33 @@
+﻿using LiteDB;
+
+namespace ModerationSystem
+{
+    public class BanSystem
+    {
+        public BanSystem()
+        {
+        }
+        public ObjectId Id { get; private set; }
+        public Player Target { get; private set; }
+        public Player Issuer { get; private set; }
+        public string Reason { get; private set; }
+        public int BanId { get; private set; }
+        public string Expire { get; private set; }
+        public string Date { get; private set; }
+        public int Duration { get; private set; }
+
+        public BanSystem(Player target, Player issuer, string reason, int duration, string date, string expire, int banid)
+        {
+            Id = ObjectId.NewObjectId();
+            Target = target;
+            Issuer = issuer;
+            Reason = reason;
+            Duration = duration;
+            Date = date;
+            Expire = expire;
+            BanId = banid;
+
+        }
+        public void Save() => Database.LiteDatabase.GetCollection<BanSystem>().Insert(this);
+    }
+}
---
+++ Warn/Database/MuteSystem.cs
@@ -0,0 +1,34 @@
+﻿using LiteDB;
+
+namespace ModerationSystem
+{
+    public class MuteSystem
+    {
+        public MuteSystem()
+        {
+        }
+        public ObjectId Id { get; private set; }
+        public Player Target { get; private set; }
+        public Player Issuer { get; private set; }
+        public string Reason { get; private set; }
+        public int MuteId { get; private set; }
+        public string Expire { get; private set; }
+        public string Date { get; private set; }
+        public int Duration { get; private set; }
+
+        public MuteSystem(Player target, Player issuer, string reason, int duration, string date, string expire, int muteid)
+        {
+            Id = ObjectId.NewObjectId();
+            Target = target;
+            Issuer = issuer;
+            Reason = reason;
+            Duration = duration;
+            Date = date;
+            Expire = expire;
+            MuteId = muteid;
+
+        }
+        public void Save() => Database.LiteDatabase.GetCollection<MuteSystem>().Insert(this);
+
+    }
+}
