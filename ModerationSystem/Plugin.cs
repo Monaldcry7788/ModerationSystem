2a3d2cd90fe98582d4add15784d1f90f203bd3f2
Monaldcry7788 <63928518+Monaldcry7788@users.noreply.github.com>
2021-09-07 18:08:29 +0200

    Update Plugin.cs

--- ModerationSystem/Plugin.cs
+++ ModerationSystem/Plugin.cs
@@ -10,7 +10,7 @@ namespace ModerationSystem
         public static Plugin Singleton;
 
         internal Events.Events Events { get; private set; }
-        public override string Author { get; } = "Twitch.tv/Monaldcry7788#9248";
+        public override string Author { get; } = "Monaldcry7788#9248";
         public override string Name { get; } = "ModerationSystem";
         public override Version Version { get; } = new Version(2, 0, 0);
         public override Version RequiredExiledVersion { get; } = new Version(3, 0, 0);
