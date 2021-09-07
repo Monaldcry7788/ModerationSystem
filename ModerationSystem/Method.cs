77ae3fb2816f397a9cc219084d34b27301e135e1
Monaldcry7788 <monaldi.christian27@gmail.com>
2021-09-07 17:39:27 +0200

    Updated

--- ModerationSystem/Method.cs
+++ ModerationSystem/Method.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using CommandSystem;
-using Exiled.API.Features;
 using MEC;
 using ModerationSystem.Collections;
 using ModerationSystem.Webhook;
